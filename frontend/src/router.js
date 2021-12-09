0fd34f026c66e53cd0022de9688931fcdd75840a
Stanislove <sta-tun-slav@yandex.ru>
2021-12-09 23:14:35 +0300

    delete old route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,39 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [{
-        path: "/",
-        component: () =>
-            import ("@/routes/main.vue"),
-    },
-    {
-        path: "/main",
-        component: () =>
-            import ("@/routes/main.vue"),
-    },
-    {
-        path: "/home",
-        component: () =>
-            import ("@/routes/main.vue"),
-    },
-    {
-        path: "/city/:city",
-        component: () =>
-            import ("@/routes/city.vue"),
-    },
-    {
-        path: "/article/:city",
-        component: () =>
-            import ("@/components/arcticle_widgets/arcticle_card_pc.vue"),
-    },
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/routes/main.vue"),
+  },
+  {
+    path: "/main",
+    component: () => import("@/routes/main.vue"),
+  },
+  {
+    path: "/home",
+    component: () => import("@/routes/main.vue"),
+  },
+  {
+    path: "/city/:city",
+    component: () => import("@/routes/city.vue"),
+  },
 ];
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHistory(),
-    routes, // short for `routes: routes`
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
 });
 
 export default router;
