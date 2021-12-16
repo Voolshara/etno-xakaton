3edc0013a093fdcb15c947aba78b9eb7444b302e
MILORD <padenev2005@yadnex.ru>
2021-12-16 18:02:05 +0300

    is compare

--- frontend/src/components/article_widgets/arcticle_card_pc.vue
+++ frontend/src/components/article_widgets/arcticle_card_pc.vue
@@ -328,7 +328,6 @@ export default {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Private-Network": true,
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: "follow", // manual, *follow, error
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
